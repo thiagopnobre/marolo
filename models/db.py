e45c2a263ed803a24b6a91dad3ec34c335cff993
thiagopnobre <thiago_pnobre@hotmail.com>
2014-03-23 15:53:49 -0300

    criando aplicação web2py

---
+++ models/db.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+db = DAL('sqlite://storage.sqlite',pool_size=1,check_reserved=['all'])
+
+## by default give a view/generic.extension to all actions from localhost
+## none otherwise. a pattern can be 'controller/function.extension'
+response.generic_patterns = ['*'] if request.is_local else []
+
+from gluon.tools import Auth
+auth = Auth(db, controller='admin', function='index')
+
+## create all tables needed by auth if not custom tables
+auth.define_tables(username=False, signature=False)
+
+## configure email
+mail = auth.settings.mailer
+mail.settings.server = 'logging' or 'smtp.gmail.com:587'
+mail.settings.sender = 'you@gmail.com'
+mail.settings.login = 'username:password'
+
+## configure auth policy
+auth.settings.registration_requires_verification = False
+auth.settings.registration_requires_approval = False
+auth.settings.reset_password_requires_verification = True
+
+## auth settings
+#auth.settings.login_next = ''
