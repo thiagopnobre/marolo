e7abfd96613e3c844993ee78f5f39e6f73c67617
cassiobotaro <cassiobotaro@gmail.com>
2015-08-24 00:43:18 -0300

    pequena melhoria, apenas um loop ao inves de 3 em apoiadores

--- models/40_globals.py
+++ models/40_globals.py
@@ -1,7 +1,11 @@
 apoiadores_geral = db(db.apoiadores).select()
-parceiros = [apoiador for apoiador in apoiadores_geral
-             if apoiador.tipo == 'parceiro']
-patrocinadores = [apoiador for apoiador in apoiadores_geral
-                  if apoiador.tipo == 'patrocinador']
-apoiadores = [apoiador for apoiador in apoiadores_geral
-              if apoiador.tipo == 'apoiador']
+parceiros = []
+patrocinadores = []
+apoiadores = []
+for apoiador in apoiadores_geral:
+    if apoiador.tipo == 'parceiro':
+        parceiros.append(apoiador)
+    elif apoiador.tipo == 'patrocinador':
+        patrocinadores.append(apoiador)
+    else:
+        apoiadores.append(apoiador)
