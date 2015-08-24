d15fd9db1e66e56c8e5327bf76eac052368c8eb8
cassiobotaro <cassiobotaro@gmail.com>
2015-08-24 00:38:05 -0300

    fixes #35 Controller com definição de modelo e alguns ajustes de pep-8

--- controllers/default.py
+++ controllers/default.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from default.contato.forms import form_contato
 
 
 def index():
@@ -63,43 +64,7 @@ def eventos():
 
 
 def contato():
-    form = FORM(
-        P(
-            INPUT(
-                _name='email',
-                _class='email',
-                _type="email",
-                _placeholder="Email",
-                _required='true'
-            )
-        ),
-        P(
-            INPUT(
-                _name="assunto",
-                _class="assunto",
-                _type="text",
-                _placeholder="Assunto",
-                _required='true'
-            )
-        ),
-        P(
-            TEXTAREA(
-                _name="mensagem",
-                _class="mensagem",
-                _placeholder="Mensagem",
-                _rows="4",
-                _required='true'
-            )
-        ),
-        P(
-            BUTTON(
-                I(_class="fa fa-envelope-o"),
-                " Enviar",
-                _class="btn btn-success btn-contato",
-                _type="submit"
-            )
-        )
-    )
+    form = form_contato()
     if form.validate():
         campos = form.vars
         mail.send(
--- populate_db.py
+++ populate_db.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#Usuário administrador
+# Usuário administrador
 db.auth_user.insert(**{
     'first_name': 'admin',
     'last_name': 'marolo',
     'email': 'marolo_admin@gmail.com',
-    'password': 'pbkdf2(1000,20,sha512)$80f5803318b24f23$abdc38a233d2a8'\
-    'c9fc611937c6269b2ab9c3ab46' # 123456
-    })
+    'password': 'pbkdf2(1000,20,sha512)$80f5803318b24f23$abdc38a233d2a8'
+    'c9fc611937c6269b2ab9c3ab46'  # 123456
+})
 
 # Notícias
 for i in xrange(11):
@@ -24,29 +24,30 @@ for i in xrange(11):
         accusamus.
         Eveniet amet eos facilis, voluptatibus temporibus nam neque ab nemo,
         et ipsum commodi fuga quibusdam eum vero soluta itaque delectus cumque,
-        quasi reprehenderit. Dignissimos natus consectetur fuga voluptatum ab sint,
-        omnis dolorum deleniti cumque quis consequuntur esse pariatur repellendus
-        ut voluptatem, consequatur dolorem et, rerum. Quibusdam aliquam ratione
-        nihil nesciunt minus dolorem, velit aperiam optio tenetur!
+        quasi reprehenderit. Dignissimos natus consectetur fuga voluptatum
+        ab sint, omnis dolorum deleniti cumque quis consequuntur esse
+        pariatur repellendus ut voluptatem, consequatur dolorem et,
+        rerum. Quibusdam aliquam ratione nihil nesciunt minus dolorem,
+        velit aperiam optio tenetur!
         ''',
         'status': 'publicado',
-        'created_by':1,
-        'modified_by':1,
-        })
+        'created_by': 1,
+        'modified_by': 1,
+    })
 
 # Membros
 for i in xrange(11):
     db.membros.insert(**{
         'nome': 'Membro {}'.format(i),
         'email': 'membro{}@email.com'.format(i)
-        })
+    })
 
 # Eventos
 for i in xrange(1, 11):
     ano = 2015
-    if i%3 == 0:
+    if i % 3 == 0:
         ano = 2014
-    elif i%5 == 0:
+    elif i % 5 == 0:
         ano = 2013
     evento = {
         'nome': 'Evento {}'.format(i),
@@ -58,7 +59,7 @@ for i in xrange(1, 11):
         elit. Porro necessitatibus facere velit labore praesentium
         maxime ullam tenetur'
         '''
-        }
+    }
     db.eventos.insert(**evento)
 
 # Apoiadores
@@ -72,4 +73,4 @@ for i in xrange(10):
         'nome': 'Apoiador {}'.format(i),
         'tipo': tipo,
         'url': 'https://en.wikipedia.org/wiki/Annona_crassiflora'
-        })
+    })
