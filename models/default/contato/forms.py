d1500872d670681075ecc1c84da522e7450a2bce
cassiobotaro <cassiobotaro@gmail.com>
2015-08-26 20:11:51 -0300

    fixes #56 Validaçoes do formulário de contato

--- models/default/contato/forms.py
+++ models/default/contato/forms.py
@@ -1,37 +1,36 @@
-form = FORM(
-    P(
-        INPUT(
-            _name='email',
-            _class='email',
-            _type="email",
-            _placeholder="Email",
-            _required='true'
-        )
+form = SQLFORM.factory(
+    Field(
+        'email',
+        requires=IS_EMAIL(
+            error_message=T('Entre com um endereço de email válido.'),
+        ),
     ),
-    P(
-        INPUT(
-            _name="assunto",
-            _class="assunto",
-            _type="text",
-            _placeholder="Assunto",
-            _required='true'
-        )
+    Field('assunto', requires=IS_IN_SET(
+        [
+            T('Reclamações'), T('Sugestões'),
+            T('Parceria'), T('Outros')
+        ],
+        zero=T('Escolha um assunto'),
+        error_message=T('Deve escolher uma opção.')
     ),
-    P(
-        TEXTAREA(
-            _name="mensagem",
-            _class="mensagem",
-            _placeholder="Mensagem",
-            _rows="4",
-            _required='true'
-        )
     ),
-    P(
-        BUTTON(
-            I(_class="fa fa-envelope-o"),
-            " Enviar",
-            _class="btn btn btn-success btn-contato",
-            _type="submit"
-        )
-    )
+
+    Field('mensagem', 'text', requires=IS_NOT_EMPTY(
+        error_message='Por favor escreva um texto.'),
+    ),
+    submit_button='{} Enviar'.format(str(I(_class="fa fa-envelope-o"))),
+    formstyle='bootstrap3_inline',
+    _class='form-horizontal'
 )
+form.element(_name="email").update(
+    _placeholder='Email',
+    _required=''
+)
+form.element(_name='mensagem').update(
+    _placeholder='Digite aqui sua mensagem.',
+    _required='',
+    _rows='4'
+)
+# form.element(_value='enviar').update(
+#     _class='btn btn-success btn-lg'
+# )
